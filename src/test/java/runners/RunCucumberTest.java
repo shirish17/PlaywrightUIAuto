ad373cf50d038abf50a8a30152e7dd6231707819
Shirish Wanjari <shirish.wanjari@gmail.com>
2026-01-13 21:33:00 +0530

    Implemented basic extent report in html and pdf.

--- src/test/java/runners/RunCucumberTest.java
+++ src/test/java/runners/RunCucumberTest.java
@@ -1,6 +1,8 @@
 package runners;
 
 
+import org.testng.annotations.DataProvider;
+
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
@@ -8,7 +10,15 @@ import io.cucumber.testng.CucumberOptions;
     features = "src/test/resources/features",  // feature folder path
     glue = {"steps"},
     //tags="@smoke or @regression",// step definition path
-    plugin = {"pretty","html:target/cucumber-report.html"}
+    plugin = {"pretty",
+    		"html:target/cucumber-report.html",
+    		"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:extent-reports/"}
 )
-public class RunCucumberTest extends AbstractTestNGCucumberTests {}
+public class RunCucumberTest extends AbstractTestNGCucumberTests {
+	@Override
+	  @DataProvider(parallel = true) // <— enables scenario-level parallelism
+	  public Object[][] scenarios() {
+	    return super.scenarios();
+	  }
+}
 
